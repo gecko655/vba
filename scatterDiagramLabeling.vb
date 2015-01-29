6b1f66e1a9249e86838f8953e64ccd1746e43b43
yamamori <yamamori@sa.cs.titech.ac.jp>
2015-01-29 15:18:39 +0900

    Flexibility improved.

--- scatterDiagramLabeling.vb
+++ scatterDiagramLabeling.vb
@@ -22,23 +22,25 @@ Sub AttachLabelsToPoints()
 
     'Attach a label to each data point in the chart.
     For Counter = 1 To Range(xVals).Cells.Count
-        ActiveChart.SeriesCollection(Series).Points(Counter).HasDataLabel = _
-          True
+      ActiveChart.SeriesCollection(Series).Points(Counter).HasDataLabel = _
+        True
+      'See the 1st row of the sheet above the x values in the sheet.
       ActiveChart.SeriesCollection(Series).Points(Counter).DataLabel.Text = _
-        Range(xVals).Cells(Counter).Offset(-2 * Series, 0).Value
+        Cells(1, Range(xVals).Cells(Counter).Column).Value
+        
     Next Counter
   Next Series
 
 End Sub
 
-' label for element 1: la1
-' x for series 1 and element 1:x11 
-' y for series 1 and element 1:y11 
 
-' Sheet structrue assumption
-' la1,la2,la3,...
-' x11,x12,x13,...
-' y11,y12,y13,...
-' x21,x22,x23,...
-' y21,y22,y23,...
+'グラフの各要素に対し、直上のセルの（その要素のx値があるセル(x,y)に対し、(1,y)のセルの）値をラベルを加えるマクロ
+'対象のグラフをアクティブにしてからマクロを実行することで利用できる。
 
+'シートの例（空行挿入可。x,yは逆順でも可）
+'label1, label2, label3,...
+'x11,x12,x13,...
+'y11,y12,y13,...
+'x21,x22,x23,...
+'y21,y22,y23,...
+'...
